4b88b408eaeb2004f8e1e17db36599ad2fbd227d
Paul Yabsley <yabsleyp@gmail.com>
2017-02-10 14:04:52 +0000

    Working with previous element and insert before

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
+const listUl = listDiv.querySelector('ul');
 const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
-const listItems = document.getElementsByTagName('li');
 
-
-listDiv.addEventListener('mouseover', (event) => {
-	if (event.target.tagName == 'LI') {
-		event.target.textContent = event.target.textContent.toUpperCase();
-	}
-});
-
-listDiv.addEventListener('mouseout', (event) => {
-	if (event.target.tagName == 'LI') {
-		event.target.textContent = event.target.textContent.toLowerCase();
+listUl.addEventListener('click', (event) => {
+	if (event.target.tagName == 'BUTTON') {
+		if (event.target.className == 'remove') {
+			let li = event.target.parentNode;
+			let ul = li.parentNode;
+			ul.removeChild(li);
+		}
+		if (event.target.className == 'up') {
+			let li = event.target.parentNode;
+			let prevLi = li.previousElementSibling;
+			let ul = li.parentNode;
+			if (prevLi) {
+				ul.insertBefore(li, prevLi);
+			}
+		}
 	}
 });
 
@@ -42,10 +46,4 @@ addItemButton.addEventListener('click', () => {
 	li.textContent = addItemInput.value;
 	ul.appendChild(li);
 	addItemInput.value = '';
-});
-
-removeItemButton.addEventListener('click', () => {
-	let ul = document.getElementsByTagName('ul')[0];
-	let li = document.querySelector('li:last-child');
-	ul.removeChild(li);
 });
