27335bc043677286454ec17e465281a670588acc
Paul Yabsley <yabsleyp@gmail.com>
2017-02-10 09:14:27 +0000

    Different methods to select things

--- 01-simple-example/app.js
+++ 01-simple-example/app.js
@@ -1,7 +1,11 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
 
-myButton.addEventListener('click', () => {
-	myHeading.style.color = myTextInput.value;
-});
+for (let i = 0; i < myList.length; i += 1) {
+	myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+	errorNotPurple[i].style.color = 'red';
+}
